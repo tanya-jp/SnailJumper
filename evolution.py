a6e2bee6358bc33922f46efbdd8df22671b49c2f
tanya-jp <t_djavaherpour@yahoo.com>
2022-06-26 13:24:06 +0430

    Add sort and crossover

--- evolution.py
+++ evolution.py
@@ -1,6 +1,7 @@
 import copy
 
 from player import Player
+import numpy as np
 
 
 class Evolution:
@@ -16,7 +17,9 @@ class Evolution:
         :param num_players: number of players that we return
         """
         # TODO (Implement top-k algorithm here)
+        sorted_players = sorted(players, key=lambda player: player.fitness, reverse=True)
         # TODO (Additional: Implement roulette wheel here)
+        # self.roulette_wheel(players, num_players)
         # TODO (Additional: Implement SUS here)
 
         # TODO (Additional: Learning curve)
@@ -35,8 +38,38 @@ class Evolution:
             return [Player(self.game_mode) for _ in range(num_players)]
         else:
             # TODO ( Parent selection and child generation )
-            new_players = prev_players  # DELETE THIS AFTER YOUR IMPLEMENTATION
+            # new_players = prev_players  # DELETE THIS AFTER YOUR IMPLEMENTATION
+            new_players = self.crossover_players(prev_players)
             return new_players
+
+    def crossover_players(self, prev_players):
+        """
+        Apply crossover on players inorder to make new children.
+        :param prev_players: List of survivors
+        :return: List of crossover children
+        """
+        new_players = []
+
+        for i in range(0, len(prev_players), 2):
+            prev_players1 = prev_players[i]
+            prev_players2 = prev_players[i + 1]
+
+            new_player1 = self.clone_player(prev_players1)
+            new_player2 = self.clone_player(prev_players2)
+
+            for i in range(len(new_player1.nn.w)):
+                shape = new_player1.nn.w[i].shape
+                # weight
+                new_player1.nn.w[i][:, shape[1] // 2:] = prev_players2.nn.w[i][:, shape[1] // 2:]
+                new_player2.nn.w[i][:, shape[1] // 2:] = prev_players1.nn.w[i][:, shape[1] // 2:]
+                # bias
+                new_player1.nn.b[i][:, shape[1] // 2:] = prev_players2.nn.b[i][:, shape[1] // 2:]
+                new_player2.nn.b[i][:, shape[1] // 2:] = prev_players1.nn.b[i][:, shape[1] // 2:]
+
+            new_players.append(new_player1)
+            new_players.append(new_player2)
+
+        return new_players
 
     def clone_player(self, player):
         """
